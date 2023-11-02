e2aa297d89c0144cea3460b393f1555470a2c10f
Jeremy Montes <jeremymontes@pursuit.org>
2023-11-02 12:06:39 -0400

    added to .env

--- db/schema.sql
+++ db/schema.sql
@@ -3,5 +3,4 @@ CREATE DATABASE colors_dev;
 
 \c colors_dev;
 
-CREATE TABLE colors(
-    id SERIAL PRIMARY KEY, name TEXT NOT NULL,is_favorite BOOLEAN);
+CREATE TABLE colors(id SERIAL PRIMARY KEY, name TEXT NOT NULL,is_favorite BOOLEAN);
